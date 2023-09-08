ec75ec8956c66bd9e450fdf57dfc8160a9d635b5
Cimorexave <sadeq.s502@gmail.com>
2023-09-08 13:14:35 +0330

    movies endpoint

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 const { log } = console;
 import * as express from "express";
+import { response, request, Response, Request } from "express";
 import * as cors from "cors";
 // const cors = require("cors");
 
@@ -11,3 +12,10 @@ app.use(express.json());
 app.listen(3030, () => {
     log(`server running on port 3030`);
 });
+
+
+// endpoints
+app.get("/movies", (req: Request, res: Response) => {
+    log(`movies requested`);
+    res.json("here are movies for you.");
+})
